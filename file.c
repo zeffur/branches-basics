4e22b7bae7d83111ee37bf901f994852c6b57bd4
zeffur <zeffurs@gmail.com>
2019-03-09 22:10:10 +0700

    File.c has been changed, commentaries are added

--- file.c
+++ file.c
@@ -4,20 +4,21 @@
 
 int main() {
   int n, i;
-  printf("Input number of elements:");
+  printf("Input number of elements:"); // ввод количества элементов в массиве
   scanf("%d", &n);
   int a[n], b[n];
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++) { //заполняем массив рандомными числами от 0 до n
     a[i] = rand() % n;
     printf("%d ", a[i]);
   }
   printf("\n");
-  for (i = 0; i < n; i++) {
+  for (i = 0; i < n; i++) { //проверяем на условие четности/нечетности
     if (a[i] % 2)
-      b[i] = pow(a[i], 3);
+      b[i] = pow(a[i], 3); //если нечетно - возводим в третью степень
     else
-      b[i] = pow(a[i], 2);
-    printf("%d \n", b[i]);
+      b[i] = pow(a[i], 2); //если четно - во вторую
+    printf("%d \n",
+           b[i]); //вывод второго массива с уже возведенными в степень числами
   }
   system("PAUSE");
   return 0;
