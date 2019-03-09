cc548941d5080899f435953471b57131cac53078
zeffur <zeffurs@gmail.com>
2019-03-09 21:54:31 +0700

    Changed file.c

---
+++ file.c
@@ -0,0 +1,24 @@
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+  int n, i;
+  printf("Input number of elements:");
+  scanf("%d", &n);
+  int a[n], b[n];
+  for (i = 0; i < n; i++) {
+    a[i] = rand() % n;
+    printf("%d ", a[i]);
+  }
+  printf("\n");
+  for (i = 0; i < n; i++) {
+    if (a[i] % 2)
+      b[i] = pow(a[i], 3);
+    else
+      b[i] = pow(a[i], 2);
+    printf("%d \n", b[i]);
+  }
+  system("PAUSE");
+  return 0;
+}
