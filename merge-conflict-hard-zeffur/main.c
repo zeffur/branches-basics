3ad2c67426f0b0b2c4b66d127720928a6837f350
zeffur <zeffurs@gmail.com>
2019-03-16 23:25:08 +0700

    Files are added

--- merge-conflict-hard-zeffur/main.c
+++ merge-conflict-hard-zeffur/main.c
@@ -18,27 +18,9 @@ int min_element(int* arr, int size)
 
 void print_array(int* arr, int size)
 {
-<<<<<<< HEAD
-    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
-
-    const int min = min_element(arr, 10);
-
-||||||| merged common ancestors
-    int arr[] = {3, 1, 4, 1, 5, 9, 2};
-
-    const int min = min_element(arr, 7);
-
-=======
->>>>>>> origin/develop
     printf("Array: ");
     int i;
-<<<<<<< HEAD
-    for (i = 0; i < 10; ++i) {
-||||||| merged common ancestors
-    for (i = 0; i < 7; ++i) {
-=======
     for (i = 0; i < size; ++i) {
->>>>>>> origin/develop
         printf("%d ", arr[i]);
     }
     printf("\n");
