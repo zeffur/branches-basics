21375f6a4e5d24a646e07636e5a194b365ca4f50
zeffur <zeffurs@gmail.com>
2019-03-17 00:34:41 +0700

    Rebase-conflict-easy normal

---
+++ rebase-conflict-easy-zeffur/main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+
+#define N_ELEMENTS(arr) (sizeof(arr) / sizeof(*(arr)))
+
+int min_element(int* arr, int size)
+{
+    int min = *arr;
+
+    int* it;
+    for (it = arr + 1; it < arr + size; ++it) {
+        if (*it < min) {
+            min = *it;
+        }
+    }
+
+    return min;
+}
+
+int main()
+{
+    int arr[] = {3, 1, 4, 1, 5, 9, 2, 6, 5, 3};
+
+    const int min = min_element(arr, N_ELEMENTS(arr));
+
+    printf("Array: ");
+    int i = 0;
+    printf("%d", arr[i]);
+    ++i;
+    for (; i < N_ELEMENTS(arr); ++i) {
+        printf(", %d", arr[i]);
+    }
+    printf("\n");
+
+    printf("Min element: %d\n", min);
+
+    return 0;
+}
